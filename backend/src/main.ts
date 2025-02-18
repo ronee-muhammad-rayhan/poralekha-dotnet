73fe46f913205c9ea3804260c898e9d080ed9bf0
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2025-02-19 00:28:07 +0600

    modify backend code

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Enable CORS
   await app.listen(4000);
-  console.log(`🚀 Server is running on http://localhost:4000`);
+  console.log(`🚀 Server is running on http://localhost:4000...`);
 }
 bootstrap();
