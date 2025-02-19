8a7985327377efb60f37eb6b7f6cb34e11d1e411
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2025-02-19 17:04:21 +0600

    update backend

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -6,6 +6,8 @@ export class UserService {
     return [
       { id: 1, name: 'John Doe', email: 'john@example.com' },
       { id: 2, name: 'Jane Smith', email: 'jane@example.com' },
+      { id: 3, name: 'John DoeNA', email: 'john@example.com' },
+      { id: 4, name: 'Jane SmithHA', email: 'jane@example.com' },
     ];
   }
 }
