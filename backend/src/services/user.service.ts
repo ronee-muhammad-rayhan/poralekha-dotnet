da9c8954f0488fdbbf2659c1c3e0f81a01799983
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2025-02-19 17:08:40 +0600

    upddate both backend and frontend

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -8,6 +8,7 @@ export class UserService {
       { id: 2, name: 'Jane Smith', email: 'jane@example.com' },
       { id: 3, name: 'John DoeNA', email: 'john@example.com' },
       { id: 4, name: 'Jane SmithHA', email: 'jane@example.com' },
+      { id: 5, name: 'RoneeMRayhan', email: 'roneemrayhan@rayhanworld.com' },
     ];
   }
 }
